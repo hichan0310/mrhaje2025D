4a6ad68ccc36819984b118c03a593067f9d36f37
hichan0310 <hichan0310@kaist.ac.kr>
2025-09-25 22:05:09 +0900

    power control example

---
+++ Assets/PlayerSystem/Effects/EffectPowerExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using EntitySystem;
+
+namespace PlayerSystem.Effects
+{
+    public class EffectPowerExample:ITriggerEffect
+    {
+        public List<ITriggerEffect> effects = new List<ITriggerEffect>();
+        
+        public void trigger(Entity entity, float power)
+        {
+            foreach (ITriggerEffect effect in effects)
+                effect.trigger(entity, power*0.7f);
+        }
+    }
+}
