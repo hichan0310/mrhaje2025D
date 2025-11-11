7ef2c674f2d5794d82e64cdb8998f9d98c10ef6b
hichan0310 <hichan0310@kaist.ac.kr>
2025-11-11 15:43:24 +0900

    asdf

--- Assets/PlayerSystem/Tiling/TestBoard.cs
+++ Assets/PlayerSystem/Tiling/TestBoard.cs
@@ -7,7 +7,7 @@ namespace PlayerSystem.Tiling
         protected override Vector3 cellPos2Real(int x, int y)
         {
             Debug.Log((x, y));
-            return new Vector3(-490+x*80, -150+y*80, 0);
+            return new Vector3(-490 + x * 80, -150 + y * 80, 0);
         }
     }
 }
